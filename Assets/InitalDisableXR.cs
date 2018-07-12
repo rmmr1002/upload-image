a9bb6f61a52dc94a8ea3ae3db2a4e3ef8bd0146c
Rohan <rmmr1002@gmail.com>
2018-07-12 16:24:46 -0400

    Master commit

---
+++ Assets/InitalDisableXR.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class InitalDisableXR : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XRSettings.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
